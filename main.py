942740fff4a6f8edc3f28cf4aeca4c68213bf8c9
tihomirtx88 <tihomirtx88@gmail.com>
2025-09-06 17:32:08 +0300

    Send email when new event is found

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
+from dotenv import load_dotenv
 
 URL = "https://programmer100.pythonanywhere.com/tours/";
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'};
 
+load_dotenv();
+
+api_key = os.getenv("API_KEY");
 
 def scrape(url):
     response = requests.get(url, headers=HEADERS);
@@ -16,8 +22,19 @@ def extraxt(source):
     value = extraxtor.extract(source)["tours"];
     return value;
 
-def send_email():
-    print("Send email");
+def send_email(message):
+    host = "smtp.gmail.com";
+    port = 465;
+
+    username = "tihpmirtx88@gmail.com";
+    password = api_key;
+
+    receiver = "tihpmirtx88@gmail.com"
+    context = ssl.create_default_context();
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 def store(extracted):
     with open("data.txt", "a") as file:
@@ -37,5 +54,5 @@ if __name__ == "__main__":
 
     if extracted not in content:
         store(extracted)
-        send_email()
+        send_email(message="New event found boy")
     print(extracted);
